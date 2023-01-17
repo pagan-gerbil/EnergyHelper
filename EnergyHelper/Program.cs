5d1a314322c0feba53d151ded6265d8293ffeb51
Pete Fullergreen <pete@pagan-gerbil.net>
2023-01-17 15:08:08 +0000

    Getting good time ranges from Octopus

--- EnergyHelper/Program.cs
+++ EnergyHelper/Program.cs
@@ -1,17 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EnergyHelper;
 using EnergyHelper.OctopusAgile;
-
-Console.WriteLine("Hello, World!");
 
 try
 {
+    var maximumPrice = 25.0;
+
     Console.WriteLine("Getting prices...");
     var octopus = new OctopusAgileApi();
     var prices = await octopus.GetStandardUnitRates();
     Console.WriteLine($"Found {prices.Results.Count()} rates");
+
+    var goodTimes = prices.Results.Where(x => x.ValueIncludingVat <= maximumPrice).OrderBy(x => x.ValidFrom).ToList();
+    Console.WriteLine($"There are {goodTimes.Count()} good times to use power");
+    
+    if (!goodTimes.Any())
+    {
+        return;
+    }
+
+    var goodTimeRanges = new List<TimeRange>();
+
+    var startTime = goodTimes[0].ValidFrom;
+    var endTime = goodTimes[0].ValidTo;
+    for (var i = 1; i < goodTimes.Count(); i++)
+    {
+        if (goodTimes[i - 1].ValidTo == goodTimes[i].ValidFrom)
+        {
+            endTime = goodTimes[i].ValidTo;
+        }
+        else
+        {
+            goodTimeRanges.Add(new TimeRange { Start = startTime, End = endTime });
+            startTime = goodTimes[i].ValidFrom;
+            endTime = goodTimes[i].ValidTo;
+        }
+    }
+    goodTimeRanges.Add(new TimeRange { Start = startTime, End = endTime });
+
+    foreach (var range in goodTimeRanges)
+    {
+        Console.WriteLine($"Good Time Range: {range.Start} to {range.End}");
+    }
 }
 catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
-
